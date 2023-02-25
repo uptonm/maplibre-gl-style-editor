cb1e5299990582f1d357efa0381f158df2552824
Mike Upton <me@uptonm.dev>
2023-02-25 18:53:11 -0500

    fix: add error boundary

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,18 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 import { Tooltip } from "react-tooltip";
+import { ErrorBoundary } from "../components/ErrorBoundary";
 import { MapEditorContextProvider } from "../context/MapEditorContext";
 
 import "../styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <MapEditorContextProvider>
-      <Component {...pageProps} />
-      <Tooltip id="action-icon-tooltip" />
-    </MapEditorContextProvider>
+    <ErrorBoundary>
+      <MapEditorContextProvider>
+        <Component {...pageProps} />
+        <Tooltip id="action-icon-tooltip" />
+      </MapEditorContextProvider>
+    </ErrorBoundary>
   );
 };
 
